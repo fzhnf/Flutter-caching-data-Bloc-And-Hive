01ccab3b8b17f8b9339f0e5298f53a5412940fad
tima gutmensch <tima_gutmensch+github@proton.me>
2025-03-12 09:21:06 +0800

    update flutter generated android folder
    - Migrate Gradle to Kotlin DSL (build.gradle.kts)
    - improvement on `gradle.properties` for optimal memory usage

--- bloc_hive_caching_data/android/app/src/main/kotlin/com/example/bloc_hive_caching_data/MainActivity.kt
+++ bloc_hive_caching_data/android/app/src/main/kotlin/com/example/bloc_hive_caching_data/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.bloc_hive_caching_data
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
